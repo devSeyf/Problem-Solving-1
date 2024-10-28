535a1664b895456d1943678fa369f66497a1a1ee
saif <ssaaiiffjjaammaall@gmail.com>
2024-10-28 12:51:04 +0300

    Add project files.

---
+++ Print Your Name/Print Your Name.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void PrintName(string Name) {
+    cout << "\n Your Name is: " << Name << endl;
+}
+
+int main()
+{
+    PrintName("saifo");
+    return 0;
+
+}
+
